7eeba6fbdac02967661131c83c93ef7fe5ca3db7
Andrew-Wong-150 <81269030+Andrew-Wong-150@users.noreply.github.com>
2022-04-18 09:22:15 -0400

    Update script.js

--- Visualizer/script.js
+++ Visualizer/script.js
@@ -1,11 +1,35 @@
-const video = document.getElementById('video')
 
-function startVideo() {
-  navigator.getUserMedia(
-    { video: {} },
-    stream => video.srcObject = stream,
-    err => console.error(err)
-  )
+function analyze(data) {
+
+	array = [];
+  rgb = [];
+
+	for(var i = 0; i < data.lenght; i++) {
+  	rgb.push(data[i]);
+    if(i % 3 == 0) {
+    	array.push(rgb);
+      rgb = [];
+    }
+  }
+
+	console.log(array[0]);
+	return array;
+
 }
 
-startVideo()
+
+var img = new Image();
+
+img.crossOrigin = 'Anonymous';
+img.onload = () => {
+
+  var context = document.createElement('canvas').getContext('2d');
+  context.drawImage(img, 0, 0);
+  var data = context.getImageData(0, 0, img.width, img.height);
+  var pixel_data = Array.prototype.slice.call(data['data']);
+
+	console.log(pixel_data);
+  
+}
+
+img.src = 'https://www.spendwithpennies.com/wp-content/uploads/2019/04/Fruit-Salad-SWP-500x375.jpg';
