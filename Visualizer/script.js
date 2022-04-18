ba47d1557fe3457b7764cbdacaac160c1966766f
Andrew-Wong-150 <81269030+Andrew-Wong-150@users.noreply.github.com>
2022-04-18 09:37:19 -0400

    Update script.js

--- Visualizer/script.js
+++ Visualizer/script.js
@@ -1,19 +1,20 @@
 
 function analyze(data) {
 
-	array = [];
-  rgb = [];
-
-	for(var i = 0; i < data.lenght; i++) {
-  	rgb.push(data[i]);
+  var array = [];
+  var rgb = [];
+   
+  console.log(Object.keys(data).length);
+  
+  for(var i = 0; i < Object.keys(data).length; i++) {
+    rgb.push(data[i]);
     if(i % 3 == 0) {
-    	array.push(rgb);
+      array.push(rgb);
       rgb = [];
     }
   }
 
-	console.log(array[0]);
-	return array;
+  return array;
 
 }
 
@@ -24,12 +25,13 @@ img.crossOrigin = 'Anonymous';
 img.onload = () => {
 
   var context = document.createElement('canvas').getContext('2d');
-  context.drawImage(img, 0, 0);
+  context.drawImage(img, 0, 0, img.width, img.height);
   var data = context.getImageData(0, 0, img.width, img.height);
   var pixel_data = Array.prototype.slice.call(data['data']);
 
-	console.log(pixel_data);
+  analyze(pixel_data);
   
 }
 
-img.src = 'https://www.spendwithpennies.com/wp-content/uploads/2019/04/Fruit-Salad-SWP-500x375.jpg';
+/*img.src = 'https://www.spendwithpennies.com/wp-content/uploads/2019/04/Fruit-Salad-SWP-500x375.jpg';*/
+img.src = 'https://picsum.photos/200/300';
