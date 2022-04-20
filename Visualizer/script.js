e43be0a8e8ec2e09c9b4b4f4de1c421e9af027f9
Andrew-Wong-150 <81269030+Andrew-Wong-150@users.noreply.github.com>
2022-04-20 09:51:07 -0400

    Update script.js

--- Visualizer/script.js
+++ Visualizer/script.js
@@ -1,37 +1,51 @@
-
 function analyze(data) {
 
-  var array = [];
   var rgb = [];
-   
-  console.log(Object.keys(data).length);
+  var length = Object.keys(data).length;
+	var chunck = 3;
   
-  for(var i = 0; i < Object.keys(data).length; i++) {
-    rgb.push(data[i]);
-    if(i % 3 == 0) {
-      array.push(rgb);
-      rgb = [];
-    }
+  for (let i = 0; i < length; i += chunck) {
+      rgb.push(data.slice(i, i + chunck));
+  }
+  
+  return rgb;
+  
+}
+
+function heatmap(data) {
+	
+  /*
+  var heat = [{
+  	z: data,
+    type: 'contour',
+    showscale: false,  
+  }];
+  */
+
+}
+
+function main() {
+
+  var img = new Image();
+
+  img.crossOrigin = 'Anonymous';
+  img.onload = () => {
+
+    var context = document.createElement('canvas').getContext('2d');
+    context.drawImage(img, 0, 0, img.width, img.height);
+    var img_data = context.getImageData(0, 0, img.width, img.height);
+    var pixel_data = Array.prototype.slice.call(img_data['data']);
+    var rgb_data = analyze(pixel_data);
+    heatmap(rgb_data);
+
   }
 
-  return array;
-
-}
-
-
-var img = new Image();
-
-img.crossOrigin = 'Anonymous';
-img.onload = () => {
-
-  var context = document.createElement('canvas').getContext('2d');
-  context.drawImage(img, 0, 0, img.width, img.height);
-  var data = context.getImageData(0, 0, img.width, img.height);
-  var pixel_data = Array.prototype.slice.call(data['data']);
-
-  analyze(pixel_data);
+  /*
+  img.src = 'https://www.spendwithpennies.com/wp-content/uploads/2019/04/Fruit-Salad-SWP-500x375.jpg';
+  img.src = 'https://picsum.photos/200/300';
+  */
+  img.src = 'https://picsum.photos/200/300';
   
 }
 
-/*img.src = 'https://www.spendwithpennies.com/wp-content/uploads/2019/04/Fruit-Salad-SWP-500x375.jpg';*/
-img.src = 'https://picsum.photos/200/300';
+main();
